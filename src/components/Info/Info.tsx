a7aba62a71bde93b11e233c26e770cb84f26da5e
Александр Карякин <dr.gr00ter@yandex.ru>
2022-09-25 16:28:18 +0300

    FEATURES
    - выполнена адаптивная вёрстка

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -9,17 +9,19 @@ const Info: FC = () => {
   return (
     <section className="info">
       <div className="info__cover">
-        <img className="info__photo" src={photo} alt="фотография профиля" />
-        <div className="info__text">
-          <h1 className="info__name">Александр Карякин</h1>
-          <p className="info__description">frontend-разработчик</p>
+        <div className="info__person">
+          <img className="info__photo" src={photo} alt="фотография профиля" />
+          <div className="info__text">
+            <h1 className="info__name">Александр Карякин</h1>
+            <p className="info__description">frontend-разработчик</p>
+          </div>
         </div>
+        <ul className="info__stack">
+          {stack.map((item, index) => (
+            <StackItem key={index} name={item} size="normal" />
+          ))}
+        </ul>
       </div>
-      <ul className="info__stack">
-        {stack.map((item, index) => (
-          <StackItem key={index} name={item} size="normal" />
-        ))}
-      </ul>
       <ul className="info__contacts">
         {contacts.map(item => (
           <Contact key={item.index} contact={item} />
